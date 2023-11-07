9c8b27c96ba40a19220c3e3116ea4467b3685eb5
eunjeong <eunjeong990731@gmail.com>
2023-11-07 17:47:08 +0900

    클릭한 위치의 위도 경도 확인

--- front/index.js
+++ front/index.js
@@ -1,6 +1,18 @@
-var mapOptions = {
-  center: new naver.maps.LatLng(37.3595704, 127.105399),
-  zoom: 10,
+let mapOptions = {
+  center: new naver.maps.LatLng(36.3503409, 127.3848208), // 대한민국 중심 좌표
+  zoom: 15, // 확대 레벨
 };
 
-var map = new naver.maps.Map('map', mapOptions);
+let map = new naver.maps.Map('map', mapOptions);
+
+naver.maps.Event.addListener(map, 'click', function (e) {
+  // 클릭한 위치의 위도와 경도를 가져옵니다.
+  var lat = e.latlng.lat();
+  var lng = e.latlng.lng();
+
+  // 이제 lat과 lng를 사용하여 원하는 작업을 수행할 수 있습니다.
+  console.log('클릭한 위치의 위도:', lat);
+  console.log('클릭한 위치의 경도:', lng);
+
+  // 커스텀 오버레이를 특정 위치에 추가하는 등의 작업을 수행할 수 있습니다.
+});
