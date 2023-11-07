3545b1b0bb2066b8a1a29b36fb8527028f73d976
eunjeong <eunjeong990731@gmail.com>
2023-11-07 16:23:27 +0900

    script 추가

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.use(express.static('front')); // 'front'는 정적 파일이 있는 디렉토리 경로입니다
+
 app.get('/', (req, res) => {
   fs.readFile('./front/index.html', (err, data) => {
     if (err) {
@@ -13,7 +15,7 @@ app.get('/', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
 });
