f9f4b747f27f897a669d4a2615f64f9da781c553
eunjeong <eunjeong990731@gmail.com>
2023-11-08 10:56:29 +0900

    대전 구 추가

--- front/index.js
+++ front/index.js
@@ -4,17 +4,17 @@ var mapOptions = {
 };
 
 var map = new naver.maps.Map('map', mapOptions);
-const mapp = document.getElementsByClassName('button')[0];
-console.log(mapp);
+const button = document.getElementsByClassName('button')[0];
+const maps = document.getElementsByClassName('mapandbutton')[0];
 
 // "btn" 버튼 엘리먼트를 가져옵니다.
 const btn = document.getElementById('btn');
 // "대전" 버튼 엘리먼트를 생성합니다.
-var daejeonButton = document.createElement('button');
+const daejeonButton = document.createElement('button');
 daejeonButton.textContent = '대전';
 
 // "광주" 버튼 엘리먼트를 생성합니다.
-var gwangjuButton = document.createElement('button');
+const gwangjuButton = document.createElement('button');
 gwangjuButton.textContent = '광주';
 
 // 버튼에 클릭 이벤트 리스너를 추가합니다.
@@ -26,7 +26,7 @@ btn.addEventListener('click', function () {
   gwangjuButton.style.display = 'none';
 
   // 클릭 이벤트를 추가합니다.
-  document.getElementById('btn').addEventListener('click', function () {
+  btn.addEventListener('click', function () {
     // "대전"과 "광주" 버튼을 토글하여 보이게 하거나 숨깁니다.
     if (daejeonButton.style.display === 'none') {
       daejeonButton.style.display = 'block';
@@ -37,6 +37,30 @@ btn.addEventListener('click', function () {
     }
   });
 
-  mapp.appendChild(daejeonButton);
-  mapp.appendChild(gwangjuButton);
+  button.appendChild(daejeonButton);
+  button.appendChild(gwangjuButton);
+});
+//구 를 나타 낼 div 생성
+const guDiv = document.createElement('div');
+maps.appendChild(guDiv);
+guDiv.style.display = 'flex';
+guDiv.style.alignItems = 'center';
+guDiv.style.flexDirection = 'column';
+
+daejeonButton.addEventListener('click', () => {
+  let jungu = document.createElement('button');
+  guDiv.appendChild(jungu);
+  jungu.textContent = '중구';
+  let seogu = document.createElement('button');
+  guDiv.appendChild(seogu);
+  seogu.textContent = '서구';
+  let dongu = document.createElement('button');
+  guDiv.appendChild(dongu);
+  dongu.textContent = '동구';
+  let daedokgu = document.createElement('button');
+  guDiv.appendChild(daedokgu);
+  daedokgu.textContent = '대덕구';
+  let yuseonggu = document.createElement('button');
+  guDiv.appendChild(yuseonggu);
+  yuseonggu.textContent = '유성구';
 });
