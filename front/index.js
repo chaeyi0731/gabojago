feb84ba2d44c6c5d1e2902fb73de2b1d5b27ae1c
eunjeong <eunjeong990731@gmail.com>
2023-11-08 17:39:17 +0900

    보이는 크기locationList.style.width 조절

--- front/index.js
+++ front/index.js
@@ -145,7 +145,7 @@ fetch('/list/daejeon.json') // list 폴더에 있는 daejeon.json 파일을 가�
       // 목록이 스크롤 가능하도록 스타일링 추가
       locationList.style.overflowY = 'auto';
       locationList.style.maxHeight = '600px';
-      locationList.style.width = '20vw';
+      locationList.style.width = '25vw';
     });
     yuseonggu
       .addEventListener('click', () => {
@@ -200,7 +200,7 @@ fetch('/list/daejeon.json') // list 폴더에 있는 daejeon.json 파일을 가�
         // 목록이 스크롤 가능하도록 스타일링 추가
         locationList.style.overflowY = 'auto';
         locationList.style.maxHeight = '600px';
-        locationList.style.width = '20vw';
+        locationList.style.width = '25vw';
       })
       .catch((error) => {
         console.error('JSON 데이터를 가져오는 중 오류가 발생했습니다:', error);
