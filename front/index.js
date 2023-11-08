50a058e1c25d3df5f043f75c42b76bb428a0d063
eunjeong <eunjeong990731@gmail.com>
2023-11-08 09:13:11 +0900

    일단 좌표 폴리고 시도 해봤으나 변경예정

--- front/index.js
+++ front/index.js
@@ -1,18 +1,58 @@
-let mapOptions = {
-  center: new naver.maps.LatLng(36.3503409, 127.3848208), // 대한민국 중심 좌표
-  zoom: 15, // 확대 레벨
+var mapOptions = {
+  center: new naver.maps.LatLng(36.3503409, 127.3848208),
+  zoom: 13, // 지도 확대 레벨을 조정하세요
 };
 
-let map = new naver.maps.Map('map', mapOptions);
+var map = new naver.maps.Map('map', mapOptions);
 
-naver.maps.Event.addListener(map, 'click', function (e) {
-  // 클릭한 위치의 위도와 경도를 가져옵니다.
-  var lat = e.latlng.lat();
-  var lng = e.latlng.lng();
+// 대전 구 경계 좌표 데이터
+var daejeonDistricts = {
+  '대전광역시/동구': [
+    new naver.maps.LatLng(36.32938, 127.44313), // 경계 좌표 1
+    // 다른 경계 좌표 추가
+  ],
+  '대전광역시/중구': [
+    new naver.maps.LatLng(36.28044, 127.41093), // 경계 좌표 1
+    // 다른 경계 좌표 추가
+  ],
+  '대전광역시/서구': [
+    new naver.maps.LatLng(36.28071, 127.34533), // 경계 좌표 1
+    // 다른 경계 좌표 추가
+  ],
+  '대전광역시/유성구': [
+    new naver.maps.LatLng(36.36685, 127.327), // 경계 좌표 1
+    // 다른 경계 좌표 추가
+  ],
+  '대전광역시/대덕구': [
+    new naver.maps.LatLng(36.39591, 127.43437), // 경계 좌표 1
+    // 다른 경계 좌표 추가
+  ],
+};
 
-  // 이제 lat과 lng를 사용하여 원하는 작업을 수행할 수 있습니다.
-  console.log('클릭한 위치의 위도:', lat);
-  console.log('클릭한 위치의 경도:', lng);
+// 각 구에 대한 다각형을 그릴 배열
+var districtPolygons = [];
 
-  // 커스텀 오버레이를 특정 위치에 추가하는 등의 작업을 수행할 수 있습니다.
-});
+for (var district in daejeonDistricts) {
+  if (daejeonDistricts.hasOwnProperty(district)) {
+    var polygon = new naver.maps.Polygon({
+      paths: daejeonDistricts[district],
+      strokeColor: '#FF0000', // 다각형 테두리 색상
+      strokeOpacity: 0.8, // 테두리 투명도
+      strokeWeight: 2, // 테두리 두께
+      fillColor: '#FF0000', // 다각형 내부 색상
+      fillOpacity: 0.35, // 내부 색상 투명도
+      clickable: true,
+      map: map,
+    });
+
+    // 다각형에 클릭 이벤트 리스너 추가
+    (function (district) {
+      naver.maps.Event.addListener(polygon, 'click', function (e) {
+        // 클릭한 구에 대한 정보를 표시하거나 원하는 작업을 수행
+        alert('클릭한 구: ' + district);
+      });
+    })(district);
+
+    districtPolygons.push(polygon);
+  }
+}
