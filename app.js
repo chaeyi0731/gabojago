258e727bd2e10bcb41eeee5306655c2b49c46b2a
eunjeong <eunjeong990731@gmail.com>
2023-11-13 16:41:51 +0900

    왜 자꾸 json만 연결이 안되는거야

--- app.js
+++ app.js
@@ -3,27 +3,34 @@ const fs = require('fs');
 const app = express();
 const path = require('path');
 
-app.use(express.static('front')); // 'front'는 정적 파일이 있는 디렉토리 경로입니다
+app.use(express.static('front'));
+
 const jsonFilePath = path.join(__dirname, 'list', 'daejeon.json');
 
 app.get('/', (req, res) => {
   fs.readFile('./front/index.html', (err, data) => {
     if (err) {
-      res.writeHead(200, 'Internal Server Error');
+      console.error('Error reading HTML file:', err);
+      res.status(500).send('Internal Server Error');
     } else {
       res.setHeader('Content-Type', 'text/html');
       res.send(data);
     }
   });
 });
-
 app.get('/list/daejeon.json', (req, res) => {
-  res.sendFile(jsonFilePath);
+  fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('서버에서 데이터를 읽는 중 오류 발생:', err);
+      res.status(500).send('서버에서 데이터를 읽는 중 오류 발생');
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(data);
+    }
+  });
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(
-    `cli 창에서 컨트롤 누른후  옆에 포트 누르면 편리하게 확인 -> http://localhost:${PORT}/`
-  );
+  console.log(`Server is running at http://localhost:${PORT}/`);
 });
