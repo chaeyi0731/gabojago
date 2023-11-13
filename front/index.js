ca9cc8be50cf4453a3a5405c974c21d1950b6f0b
eunjeong <eunjeong990731@gmail.com>
2023-11-13 15:51:42 +0900

    json 서버로 불러오는 것 ㅇ오류 ㅇ체크중

--- front/index.js
+++ front/index.js
@@ -65,6 +65,7 @@ btn.addEventListener('click', async () => {
     guDiv.innerHTML = '';
 
     // 대전 버튼이면 구 버튼 생성
+    // 대전 버튼이면 구 버튼 생성
     if (city === '대전') {
       guNames.forEach((guName) => {
         const guButton = createStyledButton(guName, async () => {
@@ -77,82 +78,82 @@ btn.addEventListener('click', async () => {
         guDiv.appendChild(guButton);
       });
     }
-
-    async function loadDistrictData(city, guName) {
-      try {
-        const response = await fetch(`/list/daejeon.json`);
-
-        if (!response.ok) {
-          throw new Error(
-            `서버 응답 오류: ${response.status} ${response.statusText}`
-          );
-        }
-
-        const data = await response.json();
-
-        if (!data || !data.locations || !Array.isArray(data.locations)) {
-          throw new Error('서버에서 올바른 데이터를 로드하지 못했습니다.');
-        }
-
-        console.log('Data from server:', data);
-        displayLocations(data.locations);
-      } catch (error) {
-        console.error('데이터 로드 중 오류가 발생했습니다:', error);
-        throw new Error('데이터 로드 중 오류가 발생했습니다.');
-      }
-    }
-
-    function displayLocations(locations) {
-      console.log('Displaying locations:', locations);
-      guDiv.innerHTML = '';
-
-      const locationList = document.createElement('ul');
-      locationList.className = 'location-list';
-
-      if (locations && Array.isArray(locations)) {
-        console.log('Displaying locations:', locations);
-        locations.forEach((location) => {
-          const listItem = document.createElement('li');
-
-          const image = document.createElement('img');
-          image.src = location.image;
-          image.alt = location.name;
-          image.style.width = '15vw';
-          listItem.appendChild(image);
-
-          const name = document.createElement('p');
-          name.textContent = `이름: ${location.name}`;
-          listItem.appendChild(name);
-
-          const address = document.createElement('p');
-          address.textContent = `주소: ${location.address}`;
-          listItem.appendChild(address);
-
-          listItem.addEventListener('click', () => {
-            console.log('ListItem clicked:', location);
-            const latitude = location.latitude;
-            const longitude = location.longitude;
-            const daejeonLocation = new naver.maps.LatLng(latitude, longitude);
-            map.setCenter(daejeonLocation);
-            marker.setPosition(daejeonLocation);
-          });
-
-          const description = document.createElement('p');
-          description.textContent = `설명: ${location.description}`;
-          listItem.appendChild(description);
-
-          locationList.appendChild(listItem);
-          listItem.style.width = '20vw';
-        });
-      }
-
-      guDiv.appendChild(locationList);
-      locationList.style.overflowY = 'auto';
-      locationList.style.maxHeight = '600px';
-      locationList.style.width = '25vw';
-    }
-
-    button.appendChild(daejeonButton);
-    button.appendChild(gwangjuButton);
   }
+
+  async function loadDistrictData(city, guName) {
+    try {
+      const response = await fetch(`/list/daejeon.json`);
+
+      if (response.status !== 200) {
+        throw new Error(
+          `서버 응답이 실패했습니다. 상태 코드: ${response.status}`
+        );
+      }
+
+      const data = await response.json();
+
+      if (!data || !data.locations || !Array.isArray(data.locations)) {
+        throw new Error('서버에서 올바른 데이터를 로드하지 못했습니다.');
+      }
+
+      console.log('Data from server:', data);
+      displayLocations(data.locations);
+    } catch (error) {
+      console.error('데이터 로드 중 오류 발생:', error.message);
+      // 여기에서 필요에 따라 추가적인 로깅이나 예외 처리를 수행할 수 있습니다.
+    }
+  }
+
+  function displayLocations(locations) {
+    console.log('Displaying locations:', locations);
+    guDiv.innerHTML = '';
+
+    const locationList = document.createElement('ul');
+    locationList.className = 'location-list';
+
+    if (locations && Array.isArray(locations)) {
+      console.log('Displaying locations:', locations);
+      locations.forEach((location) => {
+        const listItem = document.createElement('li');
+
+        const image = document.createElement('img');
+        image.src = location.image;
+        image.alt = location.name;
+        image.style.width = '15vw';
+        listItem.appendChild(image);
+
+        const name = document.createElement('p');
+        name.textContent = `이름: ${location.name}`;
+        listItem.appendChild(name);
+
+        const address = document.createElement('p');
+        address.textContent = `주소: ${location.address}`;
+        listItem.appendChild(address);
+
+        listItem.addEventListener('click', () => {
+          console.log('ListItem clicked:', location);
+          const latitude = location.latitude;
+          const longitude = location.longitude;
+          const daejeonLocation = new naver.maps.LatLng(latitude, longitude);
+          map.setCenter(daejeonLocation);
+          marker.setPosition(daejeonLocation);
+        });
+
+        const description = document.createElement('p');
+        description.textContent = `설명: ${location.description}`;
+        listItem.appendChild(description);
+
+        locationList.appendChild(listItem);
+        listItem.style.width = '20vw';
+      });
+    }
+
+    guDiv.appendChild(locationList);
+    locationList.style.overflowY = 'auto';
+    locationList.style.maxHeight = '600px';
+    locationList.style.width = '25vw';
+  }
+
+  button.appendChild(daejeonButton);
+  button.appendChild(gwangjuButton);
 });
